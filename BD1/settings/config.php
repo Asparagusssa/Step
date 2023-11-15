3bd07e6576574bce45529bcc59d7a5c94ca7689c
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2023-11-15 22:21:33 +0400

    Add files via upload

---
+++ BD1/settings/config.php
@@ -0,0 +1,12 @@
+<?php
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_password = 'root';
+    $db_name = 'Bs';
+
+    $mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
+    // $mysqli->query("SET NAMES 'utf-8'");
+
+    if($mysqli->connect_errno) {
+        echo "Не удалось подключиться к MySQL: " . $mysqli->connect_error;
+    }
